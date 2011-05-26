a735a859f56996a9fcf6782e48704510cbc8fcf2
Jacob Stanley <jacob@stanley.io>
2011-05-27 02:16:43 +0800

    Removed unnecessary include "HsUnix.h" (see ghc ticket #5172)
    http://hackage.haskell.org/trac/ghc/ticket/5172

--- include/HsUnixCompat.h
+++ include/HsUnixCompat.h
@@ -1,4 +1,3 @@
-#include "HsUnix.h"
 #include <sys/types.h>
 
 unsigned int unix_major(dev_t dev);
@@ -6,4 +5,3 @@ unsigned int unix_minor(dev_t dev);
 dev_t unix_makedev(unsigned int maj, unsigned int min);
 
 #define NEED_setSymbolicLinkOwnerAndGroup !HAVE_LCHOWN
-
