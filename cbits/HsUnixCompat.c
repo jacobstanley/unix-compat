113734269126d871878359cc493523e84ff2b43a
Jacob Stanley <jacob@stanley.io>
2011-08-17 04:13:09 +0800

    Normalized whitespace & formatting in C bits

--- cbits/HsUnixCompat.c
+++ cbits/HsUnixCompat.c
@@ -4,15 +4,17 @@
 #include <sys/mkdev.h>
 #endif
 
-unsigned int unix_major(dev_t dev) {
-  return major(dev);
+unsigned int unix_major(dev_t dev)
+{
+    return major(dev);
 }
 
-unsigned int unix_minor(dev_t dev) {
-  return minor(dev);
+unsigned int unix_minor(dev_t dev)
+{
+    return minor(dev);
 }
 
-dev_t unix_makedev(unsigned int maj, unsigned int min) {
-  return makedev(maj, min);
+dev_t unix_makedev(unsigned int maj, unsigned int min)
+{
+    return makedev(maj, min);
 }
-
