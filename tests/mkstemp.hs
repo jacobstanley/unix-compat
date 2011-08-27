59f6d08ed5ebc6adc1ecc7cea12a24d5b529f682
Jacob Stanley <jacob@stanley.io>
2011-08-28 01:19:29 +0800

    Improved mkstemp test script

--- tests/mkstemp.hs
+++ tests/mkstemp.hs
@@ -1,13 +1,27 @@
-import Control.Monad
-import System.PosixCompat
+import Control.Monad.Parallel
+import Data.List
+import System.Directory
 import System.IO
+import System.PosixCompat
 
 main :: IO ()
-main = replicateM_ 10 mktemp
+main = do
+    let n = 10000
+    hSetBuffering stdout NoBuffering
 
-mktemp :: IO ()
-mktemp = do
+    putStr $ "Creating " ++ show n ++ " temp files..."
+    xs <- replicateM n createTempFile
+    if length xs == n
+     then putStrLn "ok"
+     else putStrLn "FAIL"
+
+    putStr "Deleting temp files..."
+    mapM_ removeFile xs
+    putStrLn "ok"
+
+createTempFile :: IO FilePath
+createTempFile = do
     (p,h) <- mkstemp "tempfileXXXXXXX"
-    putStrLn $ "Created temp file: " ++ p
-    hPutStrLn h "This is a temp file"
+    hPutStrLn h "this is a temporary file"
     hClose h
+    return p
