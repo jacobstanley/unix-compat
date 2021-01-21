c89b440cdefd985bc1823cf89a077ee662b8df32
bfrk <benjamin.franksen@helmholtz-berlin.de>
2021-01-21 11:57:29 +0100

    partial support for high resolution timestamps, drop definition of FileID (#45)
    * support for reading high resolution timestamps
    
    This should also improve efficiency, since we no longer call the portable
    functions from the directory package. While we're at it, we now also take
    the file size directly from the Windows file handle meta data.
    
    * remove redefinition of System.Posix.Types.FileID
    
    * bump version to 0.5.2.1 to avoid mixup with official 0.5.2
    
    * unix-compat-0.5.3
    
    Co-authored-by: Jacob Stanley <jacob@stanley.io>

--- src/System/PosixCompat/Types.hs
+++ src/System/PosixCompat/Types.hs
@@ -8,14 +8,11 @@ On Windows 'UserID', 'GroupID' and 'LinkCount' are missing, so they are
 redefined by this module.
 -}
 module System.PosixCompat.Types (
+      module System.Posix.Types
 #ifdef mingw32_HOST_OS
-     module AllPosixTypesButFileID
-    , FileID
     , UserID
     , GroupID
     , LinkCount
-#else
-     module System.Posix.Types
 #endif
     ) where
 
@@ -23,15 +20,9 @@ module System.PosixCompat.Types (
 -- Since CIno (FileID's underlying type) reflects <sys/type.h> ino_t,
 -- which mingw defines as short int (int16), it must be overriden to
 -- match the size of windows fileIndex (word64).
-import System.Posix.Types as AllPosixTypesButFileID hiding (FileID)
+import System.Posix.Types
 
-import Data.Word (Word32, Word64)
-
-newtype FileID = FileID Word64
-  deriving (Eq, Ord, Enum, Bounded, Integral, Num, Real)
-instance Show FileID where show (FileID x) = show x
-instance Read FileID where readsPrec i s = [ (FileID x, s')
-                                           | (x,s') <- readsPrec i s]
+import Data.Word (Word32)
 
 newtype UserID = UserID Word32
   deriving (Eq, Ord, Enum, Bounded, Integral, Num, Real)
